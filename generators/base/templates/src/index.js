59d09ad3b9c01f1fead124481f58b258d09b911a
Frito <fritogotlayed@gmail.com>
2020-04-27 09:28:03 -0500

    Added basic test to stub project

--- generators/base/templates/src/index.js
+++ generators/base/templates/src/index.js
@@ -1,37 +1,43 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 
-const { logger } = require('./globals');
+const globals = require('./globals');
 const handlers = require('./handlers');
 const appShutdown = require('./handlers/app_shutdown');
 
-const app = express();
-const port = 8888;
+const buildApp = () => {
+  const logger = globals.getLogger();
+  const app = express();
 
-const requestLogger = (req, res, next) => {
-  logger.trace(`Handling ${req.path} - ${req.method}`);
-  next();
+  const requestLogger = (req, res, next) => {
+    logger.trace(`Handling ${req.path} - ${req.method}`);
+    next();
+  };
+
+  const commonResponseSetup = (req, res, next) => {
+    res.setHeader('content-type', 'application/json');
+    next();
+  };
+
+  const configureRoutes = (expressApp) => {
+    expressApp.get('/', (req, res) => {
+      // TODO: Need to create help documentation and publish it here.
+      res.send('{"msg":"Hello World!"}');
+    });
+
+    expressApp.use('/', handlers);
+  };
+
+  app.use(requestLogger);
+  app.use(commonResponseSetup);
+  app.use(bodyParser.json());
+  app.use(bodyParser.text());
+  configureRoutes(app);
+  appShutdown.wire();
+
+  return app;
 };
 
-const commonResponseSetup = (req, res, next) => {
-  res.setHeader('content-type', 'application/json');
-  next();
+module.exports = {
+  buildApp,
 };
-
-const configureRoutes = (expressApp) => {
-  expressApp.get('/', (req, res) => {
-    // TODO: Need to create help documentation and publish it here.
-    res.send('Hello World!');
-  });
-
-  expressApp.use('/', handlers);
-};
-
-app.use(requestLogger);
-app.use(commonResponseSetup);
-app.use(bodyParser.json());
-app.use(bodyParser.text());
-configureRoutes(app);
-appShutdown.wire();
-
-app.listen(port, () => logger.info(`Example app listening on port ${port}!`));
